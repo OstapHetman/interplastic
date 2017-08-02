a3b740f6a89b7dc753f61ff0be155cb4cf5c16ab
OstapHetman <prickly4528@gmail.com>
2017-08-02 23:08:56 +0300

    style map

--- js/google-map.js
+++ js/google-map.js
@@ -1,11 +1,31 @@
 function initMap() {
     // Map options
     var options = {
-            zoom: 16,
+            zoom: 17,
             center: {
-                lat: 56.378517,
-                lng: 41.307161
-            }
+                lat: 56.378999,
+                lng: 41.3097700
+            },
+            styles: [{
+                    "featureType": "road.local",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                            "color": "#ffec97"
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape.man_made",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                        "color": "#f7f1d9"
+                    }]
+                }
+            ]
+
         }
         // New map
     var map = new
@@ -18,6 +38,6 @@ function initMap() {
             lng: 41.307161
         },
         map: map,
-        icon: 'images/marker.png'
+        icon: 'images/marker.png',
     });
 }
